ea411a133045365638c6722bf20dae4bba0c4997
shidel-dev <andrewshidel@gmail.com>
2014-08-04 17:59:03 -0400

    Implmented the cacheing system and added getGroupDetail function.

--- app/Groupit/src/com/cs275/Groupit/helpers/FacebookHelper.java
+++ app/Groupit/src/com/cs275/Groupit/helpers/FacebookHelper.java
@@ -3,11 +3,17 @@
  */
 package com.cs275.Groupit.helpers;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -18,8 +24,10 @@ import com.facebook.HttpMethod;
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
+import com.facebook.model.GraphObject;
 import com.facebook.model.GraphUser;
 import com.google.gson.Gson;
+import com.google.gson.JsonParser;
 
 /**
  * 
@@ -30,20 +38,26 @@ public class FacebookHelper {
 	private static JSONArray groups;
 	private static Session session;
 	private static Map<String, Object> user;
+	private static Cache cache;
+	private static Context context;
 	//private final CountDownLatch loginLatch = new CountDownLatch (1);
 	
-	public FacebookHelper(){
-		session = getSession(null);
+	public FacebookHelper(Context _context){
+		context = _context;
+		cache = new Cache(context, "FacebookCache");
+		session = getSession();
 	}
 	
 	/**
 	 * 
 	 */
-	public FacebookHelper(Session _session) {
+	public FacebookHelper(Session _session, Context _context) {
+		context = _context;
+		cache = new Cache(context, "FacebookCache");
 		session=_session;
 	}
 	
-	public static Session getSession(Context c){
+	public static Session getSession(){
 		if (session!=null)
 			return session;
 		return Session.getActiveSession();
@@ -57,42 +71,49 @@ public class FacebookHelper {
 	 * A static version of the groups if available, or  sharedPreference if available, else, it will go to Facebook and fetch the data.
 	 * Note if this is the first time getting the users groups, then it will return null.
 	 */
-	public JSONArray getGroups(Context c, Callback...call){
-		if (groups!=null) { 
+	public void getGroups(Callback...call){
+		if (cache.get("groups")!=null) {  
 			if (call!=null)
-				call[0].finished(groups);
-			return groups;
+				call[0].finished(((SerializableJSONArray)cache.get("groups")).getJSONArray());
+		}else{
+			fetchGroups(call);
 		}
-		
-		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
-		String groupsString = sp.getString("groups", null);
-		
-		if (groupsString==null) {
-			fetchGroups(c, call);
-			return groups;
-		}
-		
-		groups = new Gson().fromJson(groupsString, JSONArray.class);
-		if (call!=null)
-			call[0].finished(groups);
-		return groups;
 	}
+	
+	public JSONObject getGroupDetails(String id, final Callback...call){
+		new Request(
+			    session,
+			    "/"+id,
+			    null,
+			    HttpMethod.GET,
+			    new Request.Callback() {
+			        public void onCompleted(Response response) {
+			        	GraphObject obj = response.getGraphObject();
+						Object thing = obj.getInnerJSONObject();
+						call[0].finished(thing);
+			        }
+			    }
+			).executeAsync();
+		
+		return null;
+	}
+	
 	@SuppressWarnings("unchecked")
-	public Map<String, Object> getUser(Context c, Callback... call){
+	public Map<String, Object> getUser(Callback... call){
 		if (user!=null)
 			return user;
-		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(c);
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
 		String userString = sp.getString("user", null);
 		if (userString==null) {
-			fetchUser(c, call);
+			fetchUser(call);
 			return user;
 		}
 		user = new Gson().fromJson(userString, Map.class);
 		return user;
 	}
 	
-	public static String getUserName(final Context c){
-		SharedPreferences prefs = c.getSharedPreferences(
+	public static String getUserName(){
+		SharedPreferences prefs = context.getSharedPreferences(
 			      "com.cs275.Groupit", Context.MODE_PRIVATE);
 		String username = prefs.getString("username", null);
 		return username;
@@ -113,7 +134,7 @@ public class FacebookHelper {
 	 * @param c
 	 * @return
 	 */
-	public void fetchGroups(final Context c, final Callback... call){
+	public void fetchGroups(final Callback... call){
 		new Request(
 				session,
          	    "/me/groups",
@@ -125,9 +146,7 @@ public class FacebookHelper {
          	        	if (response.getError()==null){
 							try {
 								groups = response.getGraphObject().getInnerJSONObject().getJSONArray("data");
-								SharedPreferences prefs = c.getSharedPreferences(
-									      "com.cs275.Groupit", Context.MODE_PRIVATE);
-								prefs.edit().putString("groups", new Gson().toJson(groups)).commit();
+								cache.add("groups", new SerializableJSONArray(groups));
 								if (call.length>0)call[0].finished(groups);
 							} catch (JSONException e) {
 								e.printStackTrace();
@@ -138,7 +157,7 @@ public class FacebookHelper {
          	).executeAsync();
 	}
 	
-	public void fetchUser(final Context c, final Callback... call){
+	public void fetchUser(final Callback... call){
 		if(!session.isOpened())
 			Log.d("Error", "Not opened!");
 	
@@ -147,7 +166,7 @@ public class FacebookHelper {
 			public void onCompleted(GraphUser _user, Response response) {
 				user=_user.asMap();
 				//loginLatch.countDown ();
-				SharedPreferences prefs = c.getSharedPreferences(
+				SharedPreferences prefs = context.getSharedPreferences(
 					      "com.cs275.Groupit", Context.MODE_PRIVATE);
 				prefs.edit().putString("user", new Gson().toJson(user));
 				if (call.length>0)call[0].finished(user);
@@ -159,5 +178,4 @@ public class FacebookHelper {
 	public interface Callback {
 	    void finished(Object g);
 	}
-	
 }
