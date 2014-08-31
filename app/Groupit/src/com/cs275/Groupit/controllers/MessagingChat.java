79d7bd02d94df550c0491419758c2f2a8698040a
Phillip Ryan <pjr87@drexel.edu>
2014-08-31 16:38:11 -0400

    Clears sent message from input view

--- app/Groupit/src/com/cs275/Groupit/controllers/MessagingChat.java
+++ app/Groupit/src/com/cs275/Groupit/controllers/MessagingChat.java
@@ -62,6 +62,7 @@ public class MessagingChat extends Controller{
 					final String message = text.getText().toString();
 					
 					create(GroupName, message);
+					text.setText("");
 					break;
 				}
 			}
