0753f4f3e80cfe46d2135abe67b27527e814e548
shidel-dev <andrewshidel@gmail.com>
2014-09-01 00:11:04 -0400

    'MyGroups' page now displays groups in a ListView

--- app/Groupit/src/com/cs275/Groupit/NavigationDrawerFragment.java
+++ app/Groupit/src/com/cs275/Groupit/NavigationDrawerFragment.java
@@ -88,6 +88,7 @@ public class NavigationDrawerFragment extends Fragment {
 		
 		
 	}
+	
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
--- app/Groupit/src/com/cs275/Groupit/controllers/MyGroups.java
+++ app/Groupit/src/com/cs275/Groupit/controllers/MyGroups.java
@@ -1,5 +1,9 @@
 package com.cs275.Groupit.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 
@@ -11,10 +15,13 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
 
 import android.app.Activity;
+import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MyGroups extends Controller{
@@ -29,25 +36,56 @@ public class MyGroups extends Controller{
 		rootView = inflator.inflate(R.layout.fragment_dashboard,
 				container, isRoot);
 		
-		final TextView v = (TextView)(rootView.findViewById(R.id.name)); 
+		
 		//Log.d("Username: ", new FacebookHelper(FacebookHelper.getSession(activity)).getUserName(activity));
 		new ServerHelper(activity).getGroupsForUser(FacebookHelper.getUserName(activity), new ServerHelper.Callback(){
 			@Override
 			public void finished(Exception e, String g) {
-				
+				TextView v = (TextView)(rootView.findViewById(R.id.name)); 
 				if (g==null || e!=null || g.equals("null")){
 					v.setText("You currently have no groups");
 					return;
 				}
 				JsonArray groups = new JsonParser().parse(g).getAsJsonArray();
-				String content = "My Groups: \n    ";
+				
+				String content = FacebookHelper.getUserName(activity)+"'s Groupits: \n    ";
+				final ArrayList<String> list = new ArrayList<String>();
 				for (int i=0; i<groups.size(); i++){
-					content+=groups.get(i).getAsString()+"\n    ";
+					list.add(groups.get(i).getAsString());
 				}
+				final StableArrayAdapter adapter = new StableArrayAdapter(activity,
+						android.R.layout.simple_list_item_1, list);
+				ListView listview = (ListView) activity.findViewById(R.id.GroupList);
+				listview.setAdapter(adapter);
 				v.setText(content);
 			} 
 		}); 
 		
 		return rootView;
 	}
+	
+	private class StableArrayAdapter extends ArrayAdapter<String> {
+
+	    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+	    public StableArrayAdapter(Context context, int textViewResourceId,
+	        List<String> objects) {
+	      super(context, textViewResourceId, objects);
+	      for (int i = 0; i < objects.size(); ++i) {
+	        mIdMap.put(objects.get(i), i);
+	      }
+	    }
+
+	    @Override
+	    public long getItemId(int position) {
+	      String item = getItem(position);
+	      return mIdMap.get(item);
+	    }
+
+	    @Override
+	    public boolean hasStableIds() {
+	      return true;
+	    }
+
+	  }
 }
